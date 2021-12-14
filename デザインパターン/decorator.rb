26fab7e052b3e554445cf7f368e5892f61640c80
RyomaKaneko <banzai.nippon2030@gmail.com>
2021-12-15 08:58:41 +0900

    feat:デザインパターン,デコレータ

--- デザインパターン/decorator.rb
+++ デザインパターン/decorator.rb
@@ -27,28 +27,28 @@ end
 
 
 
-# 複数のデコレータの共通部分(Decorator)
-# class WriterDecorator
-#   def initialize(real_writer)
-#     @real_writer = real_writer
-#   end
-#
-#   def write_line(line)
-#     @real_writer.write_line(line)
-#   end
-#
-#   def pos
-#     @real_writer.pos
-#   end
-#
-#   def rewind
-#     @real_writer.rewind
-#   end
-#
-#   def close
-#     @real_writer.close
-#   end
-# end
+複数のデコレータの共通部分(Decorator)
+class WriterDecorator
+  def initialize(real_writer)
+    @real_writer = real_writer
+  end
+
+  def write_line(line)
+    @real_writer.write_line(line)
+  end
+
+  def pos
+    @real_writer.pos
+  end
+
+  def rewind
+    @real_writer.rewind
+  end
+
+  def close
+    @real_writer.close
+  end
+end
 
 
 
