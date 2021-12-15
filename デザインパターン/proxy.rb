f6b4721d181ffed432c283f67f5fd51207fbb602
RyomaKaneko <banzai.nippon2030@gmail.com>
2021-12-16 07:08:13 +0900

    add: 取得したデータを再構築するコード

---
+++ デザインパターン/proxy.rb
@@ -0,0 +1,18 @@
+# 銀行の入出金業務を行う(対象オブジェクト/subject)
+class BankAccount
+  attr_reader :balance
+
+  def initialize(balance)
+    @balance = balance
+  end
+
+  # 出金
+  def deposit(amount)
+     balance += amount
+  end
+
+  # 入金
+  def withdraw(amount)
+     balance -= amount
+  end
+end
