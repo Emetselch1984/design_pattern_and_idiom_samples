87d879f14e743dfcc74c4bb51c92cd2bc629a4ab
RyomaKaneko <banzai.nippon2030@gmail.com>
2021-12-20 10:08:39 +0900

    feat: 【デザインパターン】オブサーバー

--- デザインパターン/practice.rb
+++ デザインパターン/practice.rb
@@ -1,68 +1,41 @@
-# 記事を表す(集約オブジェクト中の要素)
+require 'observer'
 
-class Article
-  # 記事のタイトル
-  attr_reader :title
+class Employee
+  include Observable
+  attr_reader :name,:title,:salary
 
-  def initialize(title)
+  def initialize(name,title,salary)
+    @name = name
     @title = title
+    @salary = salary
+    add_observer(Payroll.new)
+    add_observer(TaxMan.new)
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    changed
+    notify_observers(self)
   end
 end
 
-class Blog
-  attr_reader :articles
-  def initialize
-    @articles = []
+class Payroll
+  def update(changed_employee)
+    text =
+        <<TEXT
+彼の給料は#{changed_employee.salary}に変更されました。
+#{changed_employee.title}のために新しい小切手を発行します。
+TEXT
+    puts text
   end
 
-  def get_article_at(index)
-    articles[index]
-  end
-
-  def add_article(article)
-    articles << article
-  end
-
-  def length
-    articles.length
-  end
-
-  def iterator
-    BlogIterator.new(self)
+end
+class TaxMan
+  def update(changed_employee)
+    puts "#{changed_employee.name}に新しい請求書を送ります"
   end
 end
 
-class BlogIterator
-  attr_reader :blog
-  attr_accessor :index
-  ZERO = 0.freeze
+employ = Employee.new("alice","学費",300000)
+employ.salary = 250000
 
-  def initialize(blog)
-    @blog = blog
-    @index = ZERO
-  end
-
-  def has_next?
-    self.index < blog.length
-  end
-
-  def next_article
-    article = self.has_next? ? blog.get_article_at(index) : nil
-    self.index = self.index + 1
-    article
-  end
-end
-
-blog = Blog.new
-blog.add_article(Article.new("デザインパターン1"))
-blog.add_article(Article.new("デザインパターン2"))
-blog.add_article(Article.new("デザインパターン3"))
-blog.add_article(Article.new("デザインパターン4"))
-
-iterator = blog.iterator
-while iterator.has_next?
-  article = iterator.next_article
-  puts article.title
-end
-# 配列のサイズより低い間、
-# 要素を取得し、 indexを増やす
