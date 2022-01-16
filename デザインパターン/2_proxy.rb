4dc9ad70a9e1e108eea71aeecac450c0da09585f
RyomaKaneko <banzai.nippon2030@gmail.com>
2022-01-16 09:03:38 +0900

    fix: コメントの誤りの訂正 出金と入金を逆にコメントしていた

--- デザインパターン/2_proxy.rb
+++ デザインパターン/2_proxy.rb
@@ -6,12 +6,12 @@ class BankAccount
     @balance = balance
   end
 
-  # 出金
+  # 入金
   def deposit(amount)
      balance += amount
   end
 
-  # 入金
+  # 出金
   def withdraw(amount)
      balance -= amount
   end
--- デザインパターン/practice.rb
+++ デザインパターン/practice.rb
@@ -1,44 +1,12 @@
-require 'observer'
-class Employee
-  include Observable
-  attr_reader :name,:title
-  attr_accessor :salary
+class BankAccount
+  attr_reader :balance
 
-  def initialize(args)
-    @name = args[:name]
-    @title = args[:title]
-    @salary = args[:salary]
-
-    add_observer(Payroll.new)
-    add_observer(TaxMan.new)
+  def initialize(balance)
+    @balance = balance
   end
 
-  def salary=(new_salary)
-    @salary = new_salary
-    changed
-    notify_observers(self)
+  def deposit(amount)
+    balance += amount
   end
 
 end
-
-
-class TaxMan
-  def update(changed_employee)
-    puts "#{changed_employee.name}に新しい税金の請求書を送ります"
-  end
-end
-class Payroll
-
-  def update(changed_employee)
-    puts "彼の給料は#{changed_employee.salary}"
-  end
-end
-
-args = {
-    name: "foo",
-    title: "bar",
-    salary: 10000
-}
-
-foo = Employee.new(args)
-foo.salary = 50000
