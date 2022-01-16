e69e71d8c3779b618af62cae52ebbf5f451d924e
RyomaKaneko <banzai.nippon2030@gmail.com>
2022-01-16 13:32:12 +0900

    test: template_method

--- デザインパターン/practice.rb
+++ デザインパターン/practice.rb
@@ -1,37 +1,62 @@
 class Report
-  attr_reader :title,:text,:formatter
-  TEXT = %w(text1 text2 text3).freeze
+  attr_reader :text,:title
+  TITLE = "html report title".freeze
+  TEXT = ["report line 1", "report line 2", "report line 3"].freeze
 
-  def initialize(formatter)
-    @title = default_title
+  def initialize
+    @title = TITLE
     @text = TEXT
-    @formatter = formatter
-  end
-
-  def default_title
-    "report title"
   end
 
   def output_report
-    formatter.output_report(self)
+    output_start
+    output_body
+    output_end
   end
 
+  def output_start ;end
+  def output_body
+    text.each do |line|
+      output_line(line)
+    end
+  end
+
+  def output_line(line)
+    raise "Called abstract method !!"
+  end
+
+  def output_end ;end
 
 end
 
-class Formatter
-  def output_report(args)
-    raise "Called method!"
+class HTMLReport < Report
+  def output_start
+    puts ""
+  end
+
+  def output_line(line)
+    format = <<TEXT
+    #{line}
+TEXT
+    puts format
+  end
+
+  def output_end
+    puts ""
   end
 end
-
-report = Report.new(Formatter.new)
-report.title
-report.text
+report = HTMLReport.new
 report.output_report
 
-class HTMLFormatter < Formatter
-  def output_report(report)
-
+class PlaneTextReport < Report
+  def output_start
+    puts "**** #{self.text} ****"
   end
+
+  def output_line(line)
+    puts line
+  end
+
 end
+report = PlaneTextReport.new
+report.output_report
