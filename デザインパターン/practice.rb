340a5992f40ec99ba587ca7897ff8a95fd41ecfd
RyomaKaneko <banzai.nippon2030@gmail.com>
2022-01-16 09:57:20 +0900

    install: irbを1.3.7にバージョンアップ 1.3.5が文字化けしたので

--- デザインパターン/practice.rb
+++ デザインパターン/practice.rb
@@ -1,12 +1,60 @@
 class BankAccount
-  attr_reader :balance
+  attr_accessor :balance
 
   def initialize(balance)
     @balance = balance
   end
 
   def deposit(amount)
-    balance += amount
+    self.balance += amount
   end
 
+  def withdraw(amount)
+    self.balance -= amount
+  end
+
+
 end
+
+require 'etc'
+
+class BankAccountProxy
+  attr_reader :real_object,:owner_name
+  
+  def initialize(real_object,owner_name)
+    @real_object = real_object
+    @owner_name = owner_name
+  end
+  
+  def balance
+    real_object.balance
+  end
+
+  def deposit(amount)
+    check_access
+    real_object.deposit(amount)
+  end
+
+  def withdraw(amount)
+    check_access
+    real_object.withdraw(amount)
+  end
+
+  def check_access
+    puts "___________"
+    puts "#{Etc.getlogin}"
+    puts "#{owner_name}"
+    if(Etc.getlogin != owner_name)
+      puts "sucess"
+      raise "不適切なアクセスです: #{owner_name}"
+    end
+  end
+  
+end
+
+bank_account = BankAccount.new(10000)
+proxy = BankAccountProxy.new(bank_account,"foo")
+proxy.owner_name
+proxy.balance
+proxy.withdraw(10000)
+
